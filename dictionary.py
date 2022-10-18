669b6bb85c050544c4b7d23e683a410e730e6c00
pkrkotic <52660764+pkrkotic@users.noreply.github.com>
2022-10-18 14:16:41 +0200

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,8 @@
+#################################################
 
+# PICSA Version 0.1
+# By Patrick Krkotic
+
+#################################################
 
 'This file is left blank intentionally'
